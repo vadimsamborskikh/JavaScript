1fefe9e9287d97291b4727e6c7f5e96c23b1ab6d
vadimsamborskikh <vsamborskikh@yandex.ru>
2025-11-20 16:46:22 +0300

    refactor

---
+++ script.js
@@ -0,0 +1,26 @@
+const questions = [
+  {
+    number: 1,
+    text: "Что вернет typeof null?",
+    options: ["null", "object", "indefined"],
+    correct: "object",
+  },
+
+  {
+    number: 2,
+    text: "Какой из перечисленных вариантов не является типом данных в JavaScript?",
+    options: ["boolean", "string", "integer"],
+    correct: "integer",
+  },
+
+  {
+    number: 3,
+    text: "Что такое переменная в JavaScript?",
+    options: [
+      "Специальный оператор для математических вычислений.",
+      "Встроенное ключевое слово для управления потоком выполнения.",
+      "Тип данных, аналогичный массиву.",
+    ],
+    correct: "Встроенное ключевое слово для управления потоком выполнения.",
+  },
+];
