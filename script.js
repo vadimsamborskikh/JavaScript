e12afb1fe474497daf115b2108c036b612ead560
vadimsamborskikh <vsamborskikh@yandex.ru>
2025-11-23 22:01:40 +0300

    refactor

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const questions = [
   {
     number: 1,
-    text: "Что вернет typeof null?",
+    text: `Что вернет <span class="question-code">typeof null</span> в JavaScript?"`,
     options: ["null", "object", "indefined"],
     correct: "object",
   },
@@ -24,3 +24,52 @@ const questions = [
     correct: "Встроенное ключевое слово для управления потоком выполнения.",
   },
 ];
+
+function showStartScreen() {
+  const container = document.getElementById('app-container');
+  container.innerHTML = `
+  <div class="container start" id="start">
+    <button class="start-test">🚀Начать тестирование🚀</button>
+  </div>`
+
+  container.querySelector('.start-test').addEventListener('click', startTest)
+}
+
+function startTest() {
+  showQuestion(1);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  showStartScreen();
+});
+
+function showQuestion(number) {
+  const currentQuestion = questions.find(item => item.number === number);
+
+  const optionsHTML = currentQuestion.options.map((option, index) => {
+    return `<label class="option-card">
+              <input type="radio" name="answer" value="null" />
+              <div class="option-content">
+                <p class="option-number">${index + 1}</p>
+                <span class="option-text">${option}</span>
+              </div>
+            </label>`;
+  });
+
+  const container = document.getElementById('app-container');
+
+  container.innerHTML = `
+   <section class="quiz-container">
+      <div class="question">
+        <p class="question">Вопрос №${currentQuestion.number}. ${currentQuestion.text}</p>
+      </div>
+      
+      <div class="options-container">
+        ${optionsHTML}
+      </div>
+      
+       <button class="submit-btn" disabled>Подтвердить ответ</button>
+    </section>`
+}
+
+
